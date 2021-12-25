eedae108adb1595960ca209ac51f844d45d27ff7
Mohammad <moh.osama84x@gmail.com>
2021-12-25 05:44:00 +0200

    Setting up server code

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const fetch = require('node-fetch')
+
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+ //const mockAPIResponse = require('./mockAPI.js')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 
@@ -23,6 +27,16 @@ app.use(express.static('dist'))
 
 console.log(JSON.stringify(mockAPIResponse))
 
+// setting up the api 
+
+const apiKey = process.env.ApiKey
+const url = "https://api.meaningcloud.com/sentiment-2.1"
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
@@ -31,7 +45,13 @@ app.get('/test', function (req, res) {
     res.json(mockAPIResponse);
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+app.post('/api', async function(req, res) {
+    input = req.body.url;
+    console.log("input url ---- > " + input);
+    const apiUrl = `${url}key=${apiKey}&url=${input}&lang=en`
+
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    console.log("response ---- > " + data)
+    res.send(data)
 })
